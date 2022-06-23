a14d35d91c2af96cfa2c497995d3fc41b1572d0e
wlisses fernando <wlissesfernando285@gmail.com>
2022-06-23 18:23:31 -0300

    feito exercício 03 do módulo primeiros passos

---
+++ bloco_04/lógica_de_programacao_algoritimos/algorimos01.js
@@ -0,0 +1,3 @@
+let fruits = [3, 4, 10, 1, 12];
+for (var count =  0;count < fruits.length; count +=1){
+    console.log(fruits[count])
---
+++ bloco_04/modulo_introdução_primeiros_passos/prática03.js
@@ -0,0 +1,10 @@
+const primeiroNumero = 78;
+const segundoNumero = 85;
+const terceiroNumero = 36;
+
+if(primeiroNumero > segundoNumero && primeiroNumero > terceiroNumero){
+console.log('O número maior é o: ' + primeiroNumero)
+}else if(segundoNumero > primeiroNumero && segundoNumero > terceiroNumero){
+console.log('O número maior é o: ' + segundoNumero)
+}else{console.log('O maior número é o: ' + terceiroNumero)
+};
