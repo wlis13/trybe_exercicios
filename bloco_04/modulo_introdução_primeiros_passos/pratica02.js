e54f63c29ad88acf257aab904b2f4d852c90bb2f
wlisses fernando <wlissesfernando285@gmail.com>
2022-06-23 16:51:01 -0300

    feito exercício 2 do módulo primeiros passos

---
+++ bloco_04/modulo_introdução_primeiros_passos/pratica02.js
@@ -0,0 +1,7 @@
+const numeroUm = 48;
+const numeroDois = 18;
+
+if(numeroUm > numeroDois){
+console.log('O número maior é o : ' + numeroUm)
+}else{console.log('O número maior é o ' + numeroDois)
+};
