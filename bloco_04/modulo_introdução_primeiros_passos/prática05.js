1825e4df556bbb19d0f379e8af83ebc6b9ed3de4
wlisses fernando <wlissesfernando285@gmail.com>
2022-06-24 15:55:16 -0300

    feita correção no exercício05 do módulo primeiros passos

--- bloco_04/modulo_introdução_primeiros_passos/prática05.js
+++ bloco_04/modulo_introdução_primeiros_passos/prática05.js
@@ -1,8 +1,17 @@
-const numeroUm = 55;
-const numeroDois = 65;
+const numeroUm = 60;
+const numeroDois = 60;
 const numeroTres = 60;
-if(numeroUm + numeroDois + numeroTres === 180){
-    console.log(true)
-}else if(numeroUm + numeroDois + numeroTres < 180 || numeroUm + numeroDois + numeroTres > 180 ){
+let erroUm = Math.sign(numeroUm);
+let erroDois = Math.sign(numeroDois)
+let erroTres = Math.sign(numeroTres)
+if (numeroUm + numeroDois + numeroTres === 180){
+    console. log (true)
+}else if ( erroUm ===  -1 || erroDois === -1 || erroTres === -1){
+    console. log ('ERRO!')
+}else if(numeroUm + numeroDois + numeroTres != 180){
     console.log(false)
-}else{console.log("ERRO!")}
+}
+
+
+
+
