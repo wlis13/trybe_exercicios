db1d329f00c3e70184b57a25b34ff32132fece9a
wlisses fernando <wlissesfernando285@gmail.com>
2022-06-24 15:23:10 -0300

    feito exercício05 do móculo primeiros passos

---
+++ bloco_04/modulo_introdução_primeiros_passos/prática05.js
@@ -0,0 +1,8 @@
+const numeroUm = 55;
+const numeroDois = 65;
+const numeroTres = 60;
+if(numeroUm + numeroDois + numeroTres === 180){
+    console.log(true)
+}else if(numeroUm + numeroDois + numeroTres < 180 || numeroUm + numeroDois + numeroTres > 180 ){
+    console.log(false)
+}else{console.log("ERRO!")}
