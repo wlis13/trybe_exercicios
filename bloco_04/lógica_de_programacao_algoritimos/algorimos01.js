6f38f1313ae68bf1d127cea933097a802f3098a5
wlisses fernando <wlissesfernando285@gmail.com>
2022-06-24 12:28:53 -0300

    inicio do exercício 04

--- bloco_04/lógica_de_programacao_algoritimos/algorimos01.js
+++ bloco_04/lógica_de_programacao_algoritimos/algorimos01.js
@@ -1,3 +1,2 @@
-let fruits = [3, 4, 10, 1, 12];
-for (var count =  0;count < fruits.length; count +=1){
-    console.log(fruits[count])
+
+
---
+++ bloco_04/modulo_introdução_primeiros_passos/prática04.js
@@ -0,0 +1,12 @@
+const numeroUm = -45;
+const numeroDois = 12;
+
+if(numeroUm === -numeroUm){
+    console.log('negativo')
+}else{console.log('positivo')
+};
+
+if(numeroDois === -numeroDois){
+    console.log('negativo')
+}else{console.log('positivo')}
+
