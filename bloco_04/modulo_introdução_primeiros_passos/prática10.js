f8bb005bd3c5534853e8827b802218daeb88ddf7
wlisses fernando <wlissesfernando285@gmail.com>
2022-06-25 20:23:26 -0300

    concluído exercício10 de lucro e tribútos do módulo primeiros passos

--- bloco_04/modulo_introdução_primeiros_passos/prática10.js
+++ bloco_04/modulo_introdução_primeiros_passos/prática10.js
@@ -1,10 +1,10 @@
-const numberOne = Number(parseInt(prompt('digite aqui o valor de custo do produto:')));
+const numberOne = Number(parseFloat(prompt('digite aqui o valor de custo do produto:')));
 
-const numberTwo = Number(parseInt(prompt('digite aqui o valor de venda:')));
+const numberTwo = Number(parseFloat(prompt('digite aqui o valor de venda:')));
 
 let imposto = numberOne / 5;
 
-let lucro = (numberTwo - numberOne) * 1000;
+let lucro = (numberTwo - numberOne) * 1.000;
 
 
 if(numberOne < 0 || numberTwo < 0 ){
@@ -12,11 +12,17 @@ if(numberOne < 0 || numberTwo < 0 ){
     document.getElementById('resultado').innerHTML = 'Os números estão incorretos!'
 
 
-}else if(  lucro >= 1.000 && lucro < 1.000000){document.getElementById('resultado').innerHTML = 'Com os valores inseridos, o lucro será de: R$' + lucro.toFixed(2) + ' Mil Reais';
+}else if(  lucro >= 1.000 && lucro < 1.000000){document.getElementById('resultado').innerHTML = 'Com os valores inseridos, o lucro com a venda de 1000 produtos será de: R$' + lucro.toFixed(3) + ' Mil Reais';
 
 document.getElementById('result').innerHTML = 'com a venda de 1000 produtos, será pago em impostos o valor de: R$' + imposto.toFixed(3) + ' Reais';
 
 
-}else{document.getElementById('resultado').innerHTML = 'Com os valores inseridos, o lucro será de: R$' + lucro.toFixed(2) + ' Reais';
+}else if(imposto >= 1.000 && imposto < 1.000000){document.getElementById('resultado').innerHTML = 'Com os valores inseridos, o lucro com a venda de 1000 produtos será de: R$' + lucro.toFixed(3) + ' Mil Reais';
 
-document.getElementById('result').innerHTML = 'com a venda de 1000 produtos, será pago em impostos o valor de: R$' + imposto.toFixed(3) + 'Reais'}
+document.getElementById('result').innerHTML = 'com a venda de 1000 produtos, será pago em impostos o valor de: R$' + imposto.toFixed(3) + 'Mil Reais';
+
+
+}else{document.getElementById('resultado').innerHTML = 'Com os valores inseridos, o lucro com a venda de 1000 produtos será de: R$' + lucro.toFixed(3) + ' Reais';
+
+document.getElementById('result').innerHTML = 'com a venda de 1000 produtos, será pago em impostos o valor de: R$' + imposto.toFixed(3) + ' Reais'
+};
