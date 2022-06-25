b8f89de2bf2c0b58d3c58709cbffd52e8c11308f
wlisses fernando <wlissesfernando285@gmail.com>
2022-06-25 08:48:20 -0300

    feito exercício08 do módulo primeiro passos, feito um exercício extra usando uma função

---
+++ bloco_04/modulo_introdução_primeiros_passos/prática08.js
@@ -0,0 +1,48 @@
+
+
+const numeroUm = 45;
+const numeroDois = 17;
+const numeroTres = 12;
+let resultado;
+
+if(numeroUm % 2 === 0 || numeroDois % 2 === 0 || numeroTres % 2 === 0){
+
+
+resultado =  true;
+
+}else{
+
+resultado =  false;
+
+
+};console.log(resultado);
+
+
+
+
+function parimpar(p,a,d  ){
+
+let result;
+
+if(p % 2 === 0 || a % 2 === 0 || d % 2 === 0){
+
+  result = true;
+
+
+}else{ result = false;
+
+}
+return result;
+
+
+};parimpar(12,15,19);
+
+
+
+
+
+
+
+
+
+
