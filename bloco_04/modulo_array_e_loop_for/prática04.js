2a070ccac4437d30398f20af96a05f4710d2b137
wlisses fernando <wlissesfernando285@gmail.com>
2022-06-28 20:05:56 -0300

    concluído prática04 (valor maior que 20)

---
+++ bloco_04/modulo_array_e_loop_for/prática04.js
@@ -0,0 +1,30 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+let soma = 0;
+let indice = 0;
+
+for (let i of numbers){
+    soma += i;
+
+for(let index in numbers){
+indice = index
+
+}
+
+};
+
+let result = soma / indice;
+
+if(result >=  20){
+    console.log("valor maior que 20")
+}else{console.log("valor menor ou igual a 20")}
+
+
+
+
+
+
+
+
+
+
