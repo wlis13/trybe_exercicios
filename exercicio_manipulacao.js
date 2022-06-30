e7bbda7ceed36e29ef8e11487881aaf78f48811f
wlisses fernando <wlissesfernando285@gmail.com>
2022-06-30 09:21:30 -0300

    alterado o testo do paragrafo da posição [1] e alterado o background da tag main

---
+++ exercicio_manipulacao.js
@@ -0,0 +1,8 @@
+function firstparagraph(param1){
+document.getElementsByTagName('p')[1].innerText = param1
+
+};firstparagraph('acredito que vou ter o DOM');
+
+
+
+document.getElementsByClassName('main-content')[0].style.backgroundColor = 'rgb(76,164,109)'
