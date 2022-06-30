6c65aa98b60c4edaf8a6307d2e02ef7dfd987357
wlisses fernando <wlissesfernando285@gmail.com>
2022-06-30 09:25:32 -0300

    inserida função 02

--- exercicio_manipulacao.js
+++ exercicio_manipulacao.js
@@ -4,5 +4,29 @@ document.getElementsByTagName('p')[1].innerText = param1
 };firstparagraph('acredito que vou ter o DOM');
 
 
+function alterarbackground (green){
+document.getElementsByClassName('main-content')[0].style.backgroundColor = green;
+}alterarbackground('rgb(76,164,109)')
 
-document.getElementsByClassName('main-content')[0].style.backgroundColor = 'rgb(76,164,109)'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
