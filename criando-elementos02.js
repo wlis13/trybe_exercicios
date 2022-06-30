fcd38cb2664fb85b053abc61506f508d3c296474
wlisses fernando <wlissesfernando285@gmail.com>
2022-06-30 20:47:49 -0300

    cocluído primeiro exercício da parte2 (Criando Elementos)

---
+++ criando-elementos02.js
@@ -0,0 +1,29 @@
+
+
+let ondeVoceEsta = document.getElementById('elementoOndeVoceEsta');
+
+let sessaoIrma = document.createElement('section')
+
+sessaoIrma.innerText = 'esse é seu novo irmão'
+
+ondeVoceEsta.appendChild(sessaoIrma)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
