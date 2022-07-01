97986c18f51ab3023a4d5ed869b6c3a2a8c71208
wlisses fernando <wlissesfernando285@gmail.com>
2022-07-01 07:17:56 -0300

    concluído exercício03 da parte(2) criando elementos

---
+++ criando_elementos04.js
@@ -0,0 +1,7 @@
+let primeiroFilho = document.getElementById('primeiroFilhoDoFilho');
+
+
+let filho = document.createElement('p');
+filho.innerText = 'esse é o filho do primeiro filho do filho';
+
+primeiroFilho.appendChild(filho)
