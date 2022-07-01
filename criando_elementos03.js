81dc827df376c4e2792fd6ac6f1446f45f9729ad
wlisses fernando <wlissesfernando285@gmail.com>
2022-06-30 21:04:09 -0300

    cocluído exercício02 do módulo Trabalhando com elementos

---
+++ criando_elementos03.js
@@ -0,0 +1,9 @@
+
+let pai = document.getElementById('pai');
+
+
+let sessao = document.createElement('section')
+
+sessao.innerText = 'esse que é seu irmão aquele de cima é seu filho kkkk';
+
+pai.appendChild(sessao);
