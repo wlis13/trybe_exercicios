fd67ca444cc3f4f043ad9a4d8acafaf89870f93a
wlisses fernando <wlissesfernando285@gmail.com>
2022-07-03 10:59:40 -0300

    implementada a função que cria dinâmicamente um botão, e dinâmica é clicar nos dias de feriado que estão destacados com a cor verde

--- bloco_05/eventos/calendario.js
+++ bloco_05/eventos/calendario.js
@@ -66,6 +66,24 @@ for(elementos of friDay){
 };
 
 
+let container = document.getElementsByClassName('buttons-container')[0];
+let holidays = document.querySelectorAll('.holiday')[0];
+holidays.addEventListener('click',criarBotao);
+holidays.style.color = 'green';
+let holidays1 = document.querySelectorAll('.holiday')[1];
+holidays1.addEventListener('click',criarBotao)
+holidays1.style.color = 'green';
+let holidays2 = document.querySelectorAll('.holiday')[2];
+holidays2.addEventListener('click',criarBotao)
+holidays2.style.color = 'green';
+
+function criarBotao (feriados) {
+let botao = document.createElement('button');
+botao.innerText = 'feriados';
+botao.id = 'btn-holiday';
+container.appendChild(botao);
+
+};
 
 
 
