0aa1b9749b5b7c3a4c0ccbbad956ccc19a0f50c0
wlisses fernando <wlissesfernando285@gmail.com>
2022-07-03 08:25:54 -0300

    foi adicionada a classe (holiday) nos dias de feriado, e estou iniciando a inclusão da classe (friday) nas sextas-feiras

--- bloco_05/eventos/calendario.js
+++ bloco_05/eventos/calendario.js
@@ -20,17 +20,34 @@ let unorderedList = document.getElementById('days');
 let decemberDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
 
 for(element of decemberDaysList){
-let daysList = document.createElement('li');
-daysList.innerHTML = element;
-daysList.className = 'day';
-unorderedList.appendChild(daysList);
+    let itensList = document.createElement('li');
+    itensList.innerText = element;
+    itensList.className = 'day'
+    unorderedList.appendChild(itensList);
 
 }
 
+let feriados = document.querySelectorAll('.day');
+for(elemento of feriados){
+    if(elemento.innerText.includes(24)){
+        elemento.className += ' holiday'
+    }
 
+    if(elemento.innerText.includes(25)){
+        elemento.className += ' holiday'
+    }
+    if(elemento.innerText.includes(31)){
+        elemento.className += ' holiday'
+    }
 
+};
 
-
+let friDay = document.querySelectorAll('.day');
+for(elementos of friDay){
+    if(elementos.innerText.includes(04)){
+        elementos.className += ' friday'
+    }
+}
 
 
 
