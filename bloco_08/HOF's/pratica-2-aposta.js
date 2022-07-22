673e1c7110e7044bf36ee3887739986cb0dfb5a5
wlisses fernando <wlissesfernando285@gmail.com>
2022-07-22 16:17:39 -0300

    feitas alterações no arquivo prática02, foi adicionado mais  texto

--- bloco_08/HOF's/pratica-2-aposta.js
+++ bloco_08/HOF's/pratica-2-aposta.js
@@ -4,7 +4,8 @@ numAleat(Math.floor(Math.random() * (6 - 1) + 1), 4);
 };
 
 const numeroAposta = (sorteado,apostado) => {
-console.log(sorteado === apostado ? `Parabéns voçê ganhou!`:`Tente novamente`);
+console.log(sorteado === apostado ? `Parabéns voçê ganhou! O número apostado foi: ${apostado} e o sorteado foi: ${sorteado} também!!!`:`Tente novamente, o número apostado foi: ${apostado} e o sorteado foi: ${sorteado}`);
+
 };
 
 aleatorio(numeroAposta);
